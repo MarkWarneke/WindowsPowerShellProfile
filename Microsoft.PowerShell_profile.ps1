e9848219e3869a279234f0817ba7b8c3fd9345c4
Mark Warneke <mawarnek@microsoft.com>
2018-07-26 09:50:32 +0200

    initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+﻿
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Set-PSReadlineOption -BellStyle None
+Set-PSReadlineOption -EditMode Emacs
